b9b6f1d9e69790eb7aec8074327560241e29257d
David <cesarovic@seznam.cz>
2018-06-23 14:55:24 +0200

    Crossfeed and fuelpumps controls

---
+++ X-Plane Voice Control/Commands/CrossFeedControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Threading.Tasks;
+using ExtPlaneNet;
+using ExtPlaneNet.Commands;
+
+namespace X_Plane_Voice_Control.Commands
+{
+    class CrossFeedControl : ControlTemplate
+    {
+        private readonly string[] _statutes = { "on", "off" };
+
+        public CrossFeedControl(ExtPlaneInterface interface_, SpeechSynthesizer synthesizer) : base(interface_, synthesizer)
+        {
+            var apuGrammar = new GrammarBuilder();
+            apuGrammar.Append("please", 0, 1);
+            apuGrammar.Append("set", 0, 1);
+            apuGrammar.Append("crossfeed");
+            apuGrammar.Append("to", 0, 1);
+            apuGrammar.Append(new Choices(_statutes), 0, 1);
+            apuGrammar.Append("please", 0, 1);
+            Grammar = new Grammar(apuGrammar);
+            RecognitionPattern = Constants.DeserializeRecognitionPattern(apuGrammar.DebugShowPhrases);
+        }
+
+        public sealed override Grammar Grammar { get; }
+        public override string RecognitionPattern { get; }
+
+        public override void DataRefSubscribe()
+        {
+            XPlaneInterface.Subscribe<double>("laminar/B738/knobs/cross_feed_pos");
+        }
+
+        public override void OnTrigger(RecognitionResult rResult, string phrase)
+        {
+            var valueToSet = _statutes.First(phrase.Contains) == "on" ? 1 : 0;
+            XPlaneInterface.SetDataRef("laminar/B738/knobs/cross_feed_pos", valueToSet);
+            SpeechSynthesizer.SpeakAsync($"crossfeed set to {(valueToSet == 1 ? "on" : "off")}");
+        }
+    }
+}
---
+++ X-Plane Voice Control/Commands/FuelPumpsControl.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using ExtPlaneNet;
+
+namespace X_Plane_Voice_Control.Commands
+{
+    class FuelPumpsControl : ControlTemplate
+    {
+        private readonly string[] _switchStatutes = { "off", "on" };
+        private readonly string[] _fuelPumpSides = { "left", "right", "center", "all" };
+        public FuelPumpsControl(ExtPlaneInterface interface_, SpeechSynthesizer synthesizer) : base(interface_, synthesizer)
+        {
+            var fuelPump = new GrammarBuilder();
+            fuelPump.Append("please", 0, 1);
+            fuelPump.Append("set", 0, 1);
+            fuelPump.Append(new Choices(_fuelPumpSides));
+            fuelPump.Append("fuel pumps");
+            fuelPump.Append("to", 0, 1);
+            fuelPump.Append(new Choices(_switchStatutes));
+            fuelPump.Append("please", 0, 1);
+            Grammar = new Grammar(fuelPump);
+            RecognitionPattern = Constants.DeserializeRecognitionPattern(fuelPump.DebugShowPhrases);
+        }
+
+        public sealed override Grammar Grammar { get; }
+        public override string RecognitionPattern { get; }
+
+        public override void DataRefSubscribe()
+        {
+            XPlaneInterface.Subscribe<double>("laminar/B738/fuel/fuel_tank_pos_lft1");
+            XPlaneInterface.Subscribe<double>("laminar/B738/fuel/fuel_tank_pos_lft2");
+
+            XPlaneInterface.Subscribe<double>("laminar/B738/fuel/fuel_tank_pos_rgt1");
+            XPlaneInterface.Subscribe<double>("laminar/B738/fuel/fuel_tank_pos_rgt2");
+
+            XPlaneInterface.Subscribe<double>("laminar/B738/fuel/fuel_tank_pos_ctr1");
+            XPlaneInterface.Subscribe<double>("laminar/B738/fuel/fuel_tank_pos_ctr2");
+        }
+
+        public override void OnTrigger(RecognitionResult rResult, string phrase)
+        {
+            var side = _fuelPumpSides.First(phrase.Contains);
+            var positionToSet = _switchStatutes.First(phrase.Contains);
+            var positionBinary = positionToSet == "on" ? 1 : 0;
+            var sideBinary = string.Empty;
+            switch (side)
+            {
+                case "left":
+                    sideBinary = "lft";
+                    break;
+                case "right":
+                    sideBinary = "rgt";
+                    break;
+                case "center":
+                    sideBinary = "ctr";
+                    break;
+                
+            }
+
+            if (side != "all")
+            {
+                XPlaneInterface.SetDataRef($"laminar/B738/fuel/fuel_tank_pos_{sideBinary}1", positionBinary);
+                XPlaneInterface.SetDataRef($"laminar/B738/fuel/fuel_tank_pos_{sideBinary}2", positionBinary);
+            }
+            else
+            {
+                XPlaneInterface.SetDataRef("laminar/B738/fuel/fuel_tank_pos_lft1", positionBinary);
+                XPlaneInterface.SetDataRef("laminar/B738/fuel/fuel_tank_pos_lft2", positionBinary);
+                XPlaneInterface.SetDataRef("laminar/B738/fuel/fuel_tank_pos_rgt1", positionBinary);
+                XPlaneInterface.SetDataRef("laminar/B738/fuel/fuel_tank_pos_rgt2", positionBinary);
+                XPlaneInterface.SetDataRef("laminar/B738/fuel/fuel_tank_pos_ctr1", positionBinary);
+                XPlaneInterface.SetDataRef("laminar/B738/fuel/fuel_tank_pos_ctr2", positionBinary);
+            }
+            SpeechSynthesizer.SpeakAsync($"{side} fuel pumps set to {positionToSet}");
+        }
+    }
+}
